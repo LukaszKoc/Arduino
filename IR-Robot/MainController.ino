22b2d0d044e0937ddf76d89aabfbea0d17c32797
LukaszKoc <lukikoc@Gmail.com>
2019-09-01 22:49:01 +0200

    double engine works

--- IR-Robot/MainController.ino
+++ IR-Robot/MainController.ino
@@ -24,12 +24,11 @@ MotorController motor1Controller(MOTOR_1_SPREED_A_PIN, MOTOR_1_TURN_1_PIN, MOTOR
 MotorController motor2Controller(MOTOR_2_SPREED_A_PIN, MOTOR_2_TURN_1_PIN, MOTOR_2_TURN_2_PIN);
 
 void setup() {
-	Serial.begin(115200);
-	constants.setup();
-	iRController.setup();
-	displayController.setup();
+	 Serial.begin(115200);
+	// iRController.setup();
+	// displayController.setup();
+	// servoController.setup();
 	joyStick.setup();
-	servoController.setup();
 	motor1Controller.setup();
 	motor2Controller.setup();
 	Serial.print("\n\nREDY\n");
@@ -37,17 +36,30 @@ void setup() {
 
 void loop() {
 	int x, y, s;
-
 	x = joyStick.readX();
- 	y = joyStick.readY();
-	s = joyStick.readS();
-		Serial.print("S:");
-		Serial.print(s);
-		Serial.print("; \tX:");
+  	y = joyStick.readY();
+	x = x-500;
+  	y = y-500;
+  	x = x/2;
+	y = y/2;
+	    Serial.print("\n");
+		Serial.print(" X:");
 		Serial.print(x);
-		Serial.print("; \tY:");
+		Serial.print(" \tY:");
 		Serial.print(y);
-		Serial.print("\n");
-		servoController.moveToPosition(x/4);	
+	motor1Controller.setSpeed(x);
+	motor2Controller.setSpeed(y);
+	
 		delay(100);
+	// int x, y, s;
+	
+	// s = joyStick.readS();
+	// 	Serial.print("S:");
+	// 	Serial.print(s);
+	// 	Serial.print("; \tX:");
+	// 	Serial.print(x);
+	// 	Serial.print("; \tY:");
+	// 	Serial.print(y);
+	// 	Serial.print("\n");
+	// 	servoController.moveToPosition(x/4);	
 }
--- IR-Robot/MotorController.ino
+++ IR-Robot/MotorController.ino
@@ -6,39 +6,44 @@
 class MotorController {
 	private:
 		int speed_pin, input1, input2;
-		bool direction;
 	public:
 		void setup();
-		void changeDirection();
+		void changeDirection(boolean isForward);
 		void setSpeed(int speed);
 		MotorController(int speed_pin_arg, int input1_arg, int input2_arg) {
 			speed_pin = speed_pin_arg;
 			input1 = input1_arg;
 			input2 = input2_arg;
-			direction = true;
 }
 		
 }; 
 
-void MotorController::setup() {
-	pinMode(speed_pin , OUTPUT);
-	pinMode(input1, OUTPUT);
-	pinMode(input2, OUTPUT);
-}
 
-void MotorController::changeDirection() {
-	if(direction) {
+void MotorController::changeDirection(boolean isForward) {
+	
+	if(isForward) {
 		digitalWrite(input1, HIGH);
 		digitalWrite(input2, LOW);
 	} else {
 		digitalWrite(input1, LOW);
 		digitalWrite(input2, HIGH);
 	}
-	direction = !direction;
 }
-
 void MotorController::setSpeed(int speed) {
+	
+	changeDirection(speed<0);
+	if(speed<0) {speed = -speed;}
+	if(speed<50) {speed = 0;}
+	if(speed>250) {speed = 250;}
+
 	analogWrite(speed_pin, speed); // Any value between 0 and 255
 }
 
+
+void MotorController::setup() {
+	pinMode(speed_pin , OUTPUT);
+	pinMode(input1, OUTPUT);
+	pinMode(input2, OUTPUT);
+	changeDirection(true);
+}
 #endif
